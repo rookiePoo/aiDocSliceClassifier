95b84456e98e41633f249d1d230c718eb4696f43
rookiePoo <jipeng93@163.com>
2020-07-09 15:25:45 +0800

    增加召回逻辑，判断ocr类别概率大于0.5

--- main_.py
+++ main_.py
@@ -1,19 +1,36 @@
 import openpyxl
-from data_helper import  char_tokenizer
+from data_helper import  char_tokenizer, load_tokenizer
 from bilstm import BiLSTM
 import numpy as np
 import os
 from metrics import *
 import matplotlib.pyplot as plt
+import pandas as pd
 
 FIELD_SLICE_DICT = {"装运": "PORT",
              "卸货": "PORT",
-             "船名": "TXT",
+             "船名": "VESSEL",
              "合同": "NO",
              "LC": "LCNO",
              "BL": "BLNO",
              "发票": "INNO"}
 
+
+def addRes4NoRes(res_dict):
+    res_df = pd.DataFrame(res_dict)
+    region_df = res_df.groupby(["region_idx"])
+    row_idxs = []
+    for name, rdf in region_df:
+        # print("===========================")
+        # print(name)
+        # print(rdf)
+        if rdf["res"].sum() == 0:
+            maxres_i = rdf['res_prob'].idxmax()
+            maxres_class_prob = rdf.loc[maxres_i, 'class_prob']
+            maxres_row_idx = rdf.loc[maxres_i, 'row_idx']
+            if maxres_class_prob > 0.5:
+                row_idxs.append([maxres_i, maxres_row_idx])
+    return row_idxs
 
 
 def predict_excel(excel_path, pre_model):
@@ -25,14 +42,20 @@ def predict_excel(excel_path, pre_model):
              "BL": [[],[]],
              "发票": [[],[]]}
 
+    # 用于在无目标区域内寻找合理的目标
+    res_dict = {"region_idx":[], "class_prob":[],"res_prob":[],"row_idx":[], "res":[]}
+
     workbook = openpyxl.load_workbook(excel_path)
     sheet = workbook.active
     sheet.title = "test_res"
 
+
     sheet.cell(row=1, column=8, value="预测概率")
     sheet.cell(row=1, column=9, value="是否为目标切片")
+    sheet.cell(row=1, column=10, value="分类概率")
     true_label = []
     pred_rate = []
+    tokenizer = load_tokenizer('./tokenizer/tokenizer.pickle')
     for i in range(1, sheet.max_row):
         region_idx = str(sheet.cell(row=i + 1, column=1).value)
         field_type = sheet.cell(row=i + 1, column=2).value
@@ -57,18 +80,31 @@ def predict_excel(excel_path, pre_model):
 
 
             _char = "$" + field_slice_type + "\t" + ocr_info + "\n"
-            char_idx_pad = char_tokenizer([_char], './tokenizer/tokenizer_nopunc.pickle')
+
+            char_idx_pad = char_tokenizer([_char], tokenizer=tokenizer)
+            #char_idx_pad = char_tokenizer([_char], './tokenizer/tokenizer_nopunc.pickle')
 
             res = pre_model.predict_one(np.array(char_idx_pad), np.array([locs]))
-            print(res[1])
+            res_dict["region_idx"].append(region_idx)
+            res_dict["class_prob"].append(res[0][0][0])
+            res_dict["res_prob"].append(res[1][0][0])
+            res_dict["row_idx"].append(i)
+            print(res[0], res[1])
 
             sheet.cell(row=i + 1, column=8, value=str(res[1][0][0]))
             pred_rate.append(res[1][0][0])
             field_res_dict[field_type_start][1].append(res[1][0][0])
             if res[1][0][0] >= 0.5:
                 sheet.cell(row=i + 1, column=9, value="目标值")
-    # for j in range(0, 20):
-    #     sheet.cell(row=j+2, column=8, value=str(1))
+                res_dict["res"].append(1)
+            else:
+                res_dict["res"].append(0)
+            sheet.cell(row=i + 1, column=10, value=str(res[0][0][0]))
+    row_idxs = addRes4NoRes(res_dict)
+    for i, ri in row_idxs:
+        sheet.cell(row=ri + 1, column=9, value="后增目标值")
+        # 由于改过来了所以认为识别为真
+        pred_rate[i] = 1.0
 
     workbook.save(excel_path.replace('result', 'result_test'))
     print("xlsx格式表格写入数据成功！")
@@ -76,13 +112,14 @@ def predict_excel(excel_path, pre_model):
 
 if __name__ == "__main__":
 
-    excel_dir = '/Users/peng_ji/Desktop/labeled01/'
+    excel_dir = '/Users/peng_ji/Desktop/AB_train/'
     trans_id = os.listdir(excel_dir)
+    #trans_id = ['732601AB20000141']
     print(trans_id)
     all_data = []
     bilstm = BiLSTM()
     bilstm.charcnn_merge_loc_model()
-    bilstm.load_weights('./saved_model/modelnopunc-weights-55-0.96.hdf5')
+    bilstm.load_weights('./saved_model/model-weights-63-0.96.hdf5')
     true_label_all = []
     pred_rate_all = []
     field_res_all_dict = {"装运": [[], []],
@@ -139,12 +176,12 @@ if __name__ == "__main__":
     plt.plot(x, recalls, label="recall")
     plt.legend(loc='best')
     plt.show()
-
-
-    for key in metrics_dict.keys():
-        print(key, "===============================")
-        for v in metrics_dict[key]:
-            print(v)
+    #
+    #
+    # for key in metrics_dict.keys():
+    #     print(key, "===============================")
+    #     for v in metrics_dict[key]:
+    #         print(v)
 
 
 
